ddc9e95d78c09c2d2a5a562fbee4e1e6fd548629
Michael Wieland <git@selfcoders.com>
2020-08-30 15:09:38 +0200

    Fix path to ssh-runner.service

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ mkdir -p ~/.local/share/kservices5/
 mkdir -p ~/.local/share/dbus-1/services/
 
 cp ssh-runner.desktop ~/.local/share/kservices5/
-sed "s|%{BASE_DIR}|${PWD}|g" krunner-ssh.service > ~/.local/share/dbus-1/services/com.selfcoders.ssh-runner.service
+sed "s|%{BASE_DIR}|${PWD}|g" ssh-runner.service > ~/.local/share/dbus-1/services/com.selfcoders.ssh-runner.service
 
 kquitapp5 krunner
